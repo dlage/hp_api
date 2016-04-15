364161a18fa03d73ee160ab372257ac824c2d89c
Prelang Builder <builder@prelang.com>
2016-04-15 13:44:26 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Region' to allow for create, update

--- app/admin/region.rb
+++ app/admin/region.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Region do
+  permit_params :external_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
